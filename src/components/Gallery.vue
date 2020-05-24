5623cd657bcb90d74ad13ea0bc14b4b1515cd89a
Kevin Andres velasco <kevinvelassco@gmail.com>
2020-05-24 18:22:03 -0500

    pagination is added

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,9 +5,8 @@
         <img :src="img" class="img-fluid img" alt="Responsive image" />
       </div>
       <div class="col-8">
-        <h5>{{name}}</h5>
-        <h6>{{status}} - {{species}}</h6>
-        
+        <h5>{{ name }}</h5>
+        <h6>{{ status }} - {{ species }}</h6>
       </div>
     </div>
   </div>
@@ -19,20 +18,19 @@ export default {
 };
 </script>
 <style scoped>
-.character{    
-    border-radius: 5px;
-    margin-bottom:15px;
-    -webkit-box-shadow: -1px 0px 14px -6px rgba(0,0,0,0.75);
-    -moz-box-shadow: -1px 0px 14px -6px rgba(0,0,0,0.75);
-    box-shadow: -1px 0px 14px -6px rgba(0,0,0,0.75);
-
+.character {
+  border-radius: 5px;
+  margin-bottom: 15px;
+  -webkit-box-shadow: -1px 0px 14px -6px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: -1px 0px 14px -6px rgba(0, 0, 0, 0.75);
+  box-shadow: -1px 0px 14px -6px rgba(0, 0, 0, 0.75);
 }
-.content-img{
-    padding-right: 0px !important;
-    padding-left: 0px !important;    
+.content-img {
+  padding-right: 0px !important;
+  padding-left: 0px !important;
 }
-.img{
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
+.img {
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <div class="row mt-3">
       <Gallery
         v-for="charact of character"
@@ -9,6 +10,37 @@
         :species="charact.species"
       ></Gallery>
     </div>
+    <div class="row justify-content-center">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item" :class="disabledPagination.disabledPrevious">
+            <a
+              class="page-link"
+              @click="changePage(page - 1)"
+              aria-label="Anterior"
+            >
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link">
+              {{ page }}
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="page-item" :class="disabledPagination.disabledNext">
+            <a
+              class="page-link"
+              @click="changePage(page + 1)"
+              aria-label="Siguiente"
+            >
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,19 +54,41 @@ export default {
   data() {
     return {
       character: null,
-      message: null
+      message: null,
+      page: 1,
+      pages: 1
     };
+  },
+  computed: {
+    disabledPagination() {
+      return {
+        disabledPrevious: this.page === 1 ? "disabled" : "",
+        disabledNext: this.page >= this.pages ? "disabled" : ""
+      };
+    }
   },
   methods: {
     findAllcharacter() {
+      const params = {
+        page: this.page
+      };
+
       axios
-        .get("https://rickandmortyapi.com/api/character/")
+        .get("https://rickandmortyapi.com/api/character/", { params })
         .then(res => {
           this.character = res.data.results;
+          this.pages = res.data.info.pages;
+          console.log(res.data.info.pages);
         })
         .catch(error => {
           this.message = error;
         });
+    },
+    changePage(page) {
+      console.log("change " + page);
+
+      this.page = page <= 0 || page > this.pages ? this.page : page;
+      this.findAllcharacter();
     }
   },
   created() {
