c4984c97e906bda96546213a90ed64a018998453
Kevin Andres velasco <kevinvelassco@gmail.com>
2020-05-24 19:30:42 -0500

    header is added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,50 @@
 <template>
   <div>
+    <div class="row sticky-top" id="header">
+      <div class="col-12">
+        <div class="col-12">
+          <p class="text-center title">R&M Personajes</p>
+        </div>
+        <div class="row justify-content-center">
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li
+                class="page-item"
+                style="cursor:pointer"
+                :class="disabledPagination.disabledPrevious"
+              >
+                <a
+                  class="page-link"
+                  @click="changePage(page - 1)"
+                  aria-label="Anterior"
+                >
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li class="page-item active" aria-current="page">
+                <a class="page-link">
+                  {{ page }}
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li
+                class="page-item"
+                style="cursor:pointer"
+                :class="disabledPagination.disabledNext"
+              >
+                <a
+                  class="page-link"
+                  @click="changePage(page + 1)"
+                  aria-label="Siguiente"
+                >
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
     <div class="row mt-3">
       <Gallery
         v-for="charact of character"
@@ -9,36 +54,6 @@
         :status="charact.status"
         :species="charact.species"
       ></Gallery>
-    </div>
-    <div class="row justify-content-center">
-      <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item" :class="disabledPagination.disabledPrevious">
-            <a
-              class="page-link"
-              @click="changePage(page - 1)"
-              aria-label="Anterior"
-            >
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li class="page-item active" aria-current="page">
-            <a class="page-link">
-              {{ page }}
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="page-item" :class="disabledPagination.disabledNext">
-            <a
-              class="page-link"
-              @click="changePage(page + 1)"
-              aria-label="Siguiente"
-            >
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
   </div>
 </template>
@@ -78,7 +93,6 @@ export default {
         .then(res => {
           this.character = res.data.results;
           this.pages = res.data.info.pages;
-          console.log(res.data.info.pages);
         })
         .catch(error => {
           this.message = error;
@@ -96,3 +110,16 @@ export default {
   }
 };
 </script>
+<style scoped>
+#header {
+  background-color: #ffffff;
+  font-weight: 600;
+  padding: 5px;
+  -webkit-box-shadow: -1px 0px 14px -6px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: -1px 0px 14px -6px rgba(0, 0, 0, 0.75);
+  box-shadow: -1px 0px 14px -6px rgba(0, 0, 0, 0.75);
+}
+.title {
+  font-size: 30px;
+}
+</style>
